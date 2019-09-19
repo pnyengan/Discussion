99aa6891c350729ae4966f391d9e2d7f9c9a030d
Phikolomzi <phikolomzinyengane@gmail.com>
2019-09-19 13:22:05 +0200

    adding password hashing

--- discuss/priv/repo/migrations/20161024175806_add_topics.exs
+++ discuss/priv/repo/migrations/20161024175806_add_topics.exs
@@ -4,6 +4,7 @@ defmodule Discuss.Repo.Migrations.AddTopics do
   def change do
     create table(:topics) do
       add :title, :string
+      add :user_id, :integer, references("users")
     end
   end
 end
